53a618ea9f9f90058474b34b98ab1cffc3d556e4
sasirekha06 <45126476+sasirekha06@users.noreply.github.com>
2023-01-13 11:51:15 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'hello new change';
+    return 'hello new change2';
   }
 }
